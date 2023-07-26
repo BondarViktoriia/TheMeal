46b69398fa5bb2d380fafb18833ad988e2dd0d26
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-07-26 14:48:02 +0300

    create header markup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,37 @@
 
 <template>
-  <router-view />
+  <div bg-gray-100 h-full>
+    <header class="bg-white shadow-md h-16 flex justify-between items-stretch">
+      <router-link
+        :to="{ name: 'home' }"
+        class="px-2 h-full hover:bg-purple-200 inline-flex items-center"
+      >
+        Home
+      </router-link>
+      <div class="flex items-center gap-1">
+        <router-link
+          to="/"
+          class="px-2 h-full hover:bg-purple-200 inline-flex items-center"
+          >Search Meals</router-link
+        >
+        <router-link
+          to="/"
+          class="px-2 h-full hover:bg-purple-200 inline-flex items-center"
+        >
+          Meals By Letter</router-link
+        >
+        <router-link
+          to="/"
+          class="px-2 h-full hover:bg-purple-200 inline-flex items-center"
+        >
+          Meal By Ingredients</router-link
+        >
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script setup>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,10 +20,17 @@
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient";
 
-const meals = computed(() => store.state.meals);
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log(response.data);
+  ingredients.value = response.data.meals;
+});
 </script>
 
