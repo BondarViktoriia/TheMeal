c2b23d0195b283e5023ab1736f5d89d4f36a53eb
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-08-15 16:46:51 +0300

    add component MealItem

---
+++ src/components/MealItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-white shadow rounded-xl">
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl" />
+    <div class="p-3">
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <p class="my-1">{{ meal.strCategory }}</p>
+    </div>
+    <div class="p-3 flex items-center justify-between">
+      <YoutubeButton :href="meal.strYoutube" />
+      <router-link
+        :to="{ name: 'mealDetails', params: { id: meal.idMeal } }"
+        class="px-3 py-2 rounded border-2 text-white bg-blue-600 border-blue-600 hover:bg-blue-500 hover:text-white transition-colors"
+        >View</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import YoutubeButton from "../components/YoutubeButton.vue";
+
+const { meal } = defineProps({
+  meal: Object,
+});
+</script>
+
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -9,25 +9,7 @@
     />
   </div>
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <div
-      v-for="meal of meals"
-      :key="meal.idMeal"
-      class="bg-white shadow rounded-xl"
-    >
-      <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl" />
-      <div class="p-3">
-        <h3 class="font-bold">{{ meal.strMeal }}</h3>
-        <p class="my-1">{{ meal.strCategory }}</p>
-      </div>
-      <div class="p-3 flex items-center justify-between">
-        <YoutubeButton :href="meal.strYoutube" />
-        <router-link
-          :to="{ name: 'mealDetails', params: { id: meal.idMeal } }"
-          class="px-3 py-2 rounded border-2 text-white bg-blue-600 border-blue-600 hover:bg-blue-500 hover:text-white transition-colors"
-          >View</router-link
-        >
-      </div>
-    </div>
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
 </template>
 
@@ -35,7 +17,7 @@
 import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import { useRoute } from "vue-router";
-import YoutubeButton from "../components/YoutubeButton.vue";
+import MealItem from "../components/MealItem.vue";
 
 const keyWord = ref("");
 const meals = computed(() => store.state.searchMeals);
